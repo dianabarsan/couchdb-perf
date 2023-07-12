94004ae6e53d9312b636a1b84f4cb1dc0c35855f
Diana Barsan <barsan@medic.org>
2023-07-12 22:29:25 +0300

    run couch3 first

--- run.sh
+++ run.sh
@@ -6,6 +6,14 @@ docker compose -f ./docker-compose/couchdb-2.yml down
 docker compose -f ./docker-compose/couchdb-3.yml down
 mkdir -p output
 
+couch3_output=couch3-"$nbr_cpus"_cpu.csv
+couch3_data=$(mktemp -d -t couchdb-XXXXXXXXXX)
+COUCHDB_DATA=$couch3_data NBR_CPU="$nbr_cpus" docker compose -f ./docker-compose/couchdb-3.yml up -d
+sleep 5
+node ./src/index.js $couch3_output
+docker compose -f ./docker-compose/couchdb-3.yml down
+
+
 couch2_output=couch2-"$nbr_cpus"_cpu.csv
 couch2_data=$(mktemp -d -t couchdb-XXXXXXXXXX)
 COUCHDB_DATA=$couch2_data NBR_CPU="$nbr_cpus" docker compose -f ./docker-compose/couchdb-2.yml up -d
@@ -13,12 +21,5 @@ sleep 5
 node ./src/index.js $couch2_output
 docker compose -f ./docker-compose/couchdb-2.yml down
 
-couch3_output=couch3-"$nbr_cpus"_cpu.csv
-couch3_data=$(mktemp -d -t couchdb-XXXXXXXXXX)
-COUCHDB_DATA=$couch3_data NBR_CPU="$nbr_cpus" docker compose -f ./docker-compose/couchdb-3.yml up -d
-sleep 5
-node ./src/index.js $couch3_output
-
-docker compose -f ./docker-compose/couchdb-3.yml down
 
 node ./src/compare-results.js $couch2_output $couch3_output merge-"$nbr_cpus"_cpu.csv
